75a6b055dc6c1e09444fd208277c11125810da9c
ydvffg <ydvffg@gmail.com>
2025-12-07 22:46:38 +0000

    asd

--- src/pages/datenschutz.tsx
+++
@@ -1,5 +0,0 @@
----
-Datenschutzerklärung
-
-Wir betreiben unseren Webserver (nginx) ohne jegliche Speicherung von personenbezogenen Daten, Logfiles oder sonstigen Tracking-Maßnahmen. Es werden keine Cookies gesetzt oder ausgewertet. Es erfolgt keinerlei Nutzeranalyse – Ihre Privatsphäre bleibt vollumfänglich geschützt.
----
